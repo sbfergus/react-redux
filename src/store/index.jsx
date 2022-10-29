3baca887d1afa8fa59bd43ad00b5476a353e19d5
Scott <sbferguson17@gmail.com>
2022-10-28 19:18:19 -0600

    activate add 10 button

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -7,6 +7,9 @@ const reducerFn = (state = { counter: 0 }, action) => {
   if (action.type === "DEC") {
     return { counter: state.counter - 1 };
   }
+  if (action.type === "ADD") {
+    return { counter: state.counter + action.payload };
+  }
 
   return state;
 };
